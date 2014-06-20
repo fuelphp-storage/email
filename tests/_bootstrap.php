e07a816280e2f1345be3259f709e0ab634c20e73
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-06-20 10:11:18 +0200

    Moves autoloading to global bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,11 @@
 <?php
-// This is global bootstrap for autoloading 
+// This is global bootstrap for autoloading
+
+require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/stubs/DummyTransport.php';
+
+// SMTP stubs
+require_once __DIR__.'/stubs/Smtp/DummyAuthentication.php';
+require_once __DIR__.'/stubs/Smtp/DummyCommand.php';
+require_once __DIR__.'/stubs/Smtp/DummySimpleCommand.php';
+require_once __DIR__.'/stubs/Smtp/DummyAddressCommand.php';
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,10 +1,1 @@
 <?php
-
-require __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/../stubs/DummyTransport.php';
-
-// SMTP stubs
-require_once __DIR__.'/../stubs/Smtp/DummyAuthentication.php';
-require_once __DIR__.'/../stubs/Smtp/DummyCommand.php';
-require_once __DIR__.'/../stubs/Smtp/DummySimpleCommand.php';
-require_once __DIR__.'/../stubs/Smtp/DummyAddressCommand.php';
